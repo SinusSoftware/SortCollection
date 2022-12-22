54d5a619215f78b2c834b34184f7f97ddc226349
Spoisege <nordseee@web.de>
2022-12-22 13:40:29 +0100

    Frame TimSort

---
+++ SortCollection/TimSort.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace System
+{
+    /// <summary>
+    /// Wraps static sort apis into a uniform extension method api.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public static class TimSort
+    {
+        [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<T> SortWithTimSort<T>(this IEnumerable<T> source)
+        {
+            return source;
+            //return SortWithBubbleSort(source, 0, source.Count(), Comparer<T>.Default);
+        }
+    }
+}
---
+++ SortCollectionUnitTest/TimSort.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortCollectionUnitTest
+{
+    [TestClass]
+    public class TimSort
+    {
+        private List<int> integers = new();
+        private List<int> randomIntegers = new();
+        private List<string> greekAlphabet = new();
+        private List<Car> cars = new();
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            integers = SupportSortingTest.GenerateSmallIntegers();
+            randomIntegers = SupportSortingTest.CreateRandomArray(10000, 1, 10000).ToList();
+            greekAlphabet = SupportSortingTest.GenerateGreekAlphabet();
+            cars = SupportSortingTest.GenerateCars();
+        }
+
+        [TestMethod]
+        public void TimSortTest()
+        {
+            var sortedList = integers.SortWithTimSort();
+
+            string test = "";
+        }
+    }
+}
